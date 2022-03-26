fec7d1137527a5d8be663815bc8a181662a5445d
JamesCurtis <49338067+james-curtis@users.noreply.github.com>
2022-03-26 21:06:04 +0800

    修复

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-import '@/assets/icons'
+import '@/assets/icons';
 
 Vue.use(element)
 
