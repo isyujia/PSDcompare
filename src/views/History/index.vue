42c2b40d77f4a04e857c78019e982a83fb3ad722
JamesCurtis <49338067+james-curtis@users.noreply.github.com>
2022-03-26 22:41:53 +0800

    完善表格

--- src/views/History/index.vue
+++ src/views/History/index.vue
@@ -20,7 +20,7 @@
             v-model="daterange"
             type="daterange"
             unlink-panels
-            range-separator="至"
+            range-separator="~"
             start-placeholder="开始日期"
             end-placeholder="结束日期"
           >
@@ -33,11 +33,56 @@
     </el-header>
     <el-main class="main">
       <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="date" label="日期" width="180">
+        <el-table-column
+          prop="serial_number"
+          label="流水编号"
+          width="200"
+        ></el-table-column>
+        <el-table-column
+          prop="compare_file_1_name"
+          label="对比文件"
+          width="200"
+        ></el-table-column>
+        <el-table-column prop="comparison_cost" label="支付费用" width="200">
         </el-table-column>
-        <el-table-column prop="name" label="姓名" width="180">
+        <el-table-column prop="status" label="支付状态" width="200">
+          <template slot-scope="scope">
+            <span v-if="scope.row.status" class="dot"
+              ><span class="blue-dot"></span>已支付</span
+            >
+            <span v-else class="dot"><span class="red-dot"></span>未支付</span>
+          </template>
         </el-table-column>
-        <el-table-column prop="address" label="地址"> </el-table-column>
+        <el-table-column prop="status" label="支付状态" width="200">
+          <template slot-scope="scope">
+            <span v-if="scope.row.status" class="dot"
+              ><span class="blue-dot"></span>已完成</span
+            >
+            <span v-else class="dot"><span class="red-dot"></span>未完成</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="status" label="支付结果" width="200">
+          <template slot-scope="scope">
+            <span v-if="scope.row.status" style="color: #67c23a">成功</span>
+            <span v-else style="color: #f56760">错误</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="create_time" label="时间" width="200">
+        </el-table-column>
+        <el-table-column label="操作" width="200">
+          <template slot-scope="scope">
+            <div class="act">
+              <svg-icon icon-class="show-eye"></svg-icon>
+              <span>查看</span>
+              <el-divider direction="vertical"></el-divider>
+              <svg-icon icon-class="download"></svg-icon>
+              <span>下载</span>
+              <el-divider direction="vertical"></el-divider>
+              <svg-icon icon-class="bin"></svg-icon>
+              <span>删除</span>
+            </div>
+          </template>
+        </el-table-column>
       </el-table>
     </el-main>
     <el-footer>
@@ -64,6 +109,14 @@ export default {
   name: "HistoryIndex",
   components: {},
   computed: {},
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
+  },
   data() {
     return {
       publicPath: process.env.BASE_URL,
@@ -71,29 +124,50 @@ export default {
       searchText: "",
       tableData: [
         {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
+          serial_number: "15881206583",
+          compare_file_1_name: "金天宇",
+          comparison_cost: "22854.87",
+          status: false,
+          create_time: "2000年09月01日 22:08",
         },
         {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
+          serial_number: "15881206583",
+          compare_file_1_name: "金天宇",
+          comparison_cost: "22854.87",
+          status: false,
+          create_time: "2000年09月01日 22:08",
         },
         {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
+          serial_number: "15881206583",
+          compare_file_1_name: "金天宇",
+          comparison_cost: "22854.87",
+          status: false,
+          create_time: "2000年09月01日 22:08",
         },
         {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
+          serial_number: "15881206583",
+          compare_file_1_name: "金天宇",
+          comparison_cost: "22854.87",
+          status: false,
+          create_time: "2000年09月01日 22:08",
+        },
+        {
+          serial_number: "15881206583",
+          compare_file_1_name: "金天宇",
+          comparison_cost: "22854.87",
+          status: false,
+          create_time: "2000年09月01日 22:08",
+        },
+        {
+          serial_number: "15881206583",
+          compare_file_1_name: "金天宇",
+          comparison_cost: "22854.87",
+          status: false,
+          create_time: "2000年09月01日 22:08",
         },
       ],
     };
   },
-  methods: {},
 };
 </script>
 
@@ -114,8 +188,37 @@ export default {
   }
   .main {
     background: white;
-    .el-table{
-        padding: 0 17px;
+    .el-table {
+      padding: 0 17px;
+      .act {
+        display: flex;
+        align-items: center;
+        line-height: 100%;
+        .svg-icon {
+          width: 1.3em;
+          height: 1.3em;
+        }
+      }
+      .dot {
+        position: relative;
+        .red-dot,
+        .blue-dot {
+          position: absolute;
+          top: 50%;
+          left: -20%;
+          width: 5px;
+          height: 5px;
+          display: inline-block;
+          border-radius: 50%;
+          transform: translateY(-25%);
+        }
+        .red-dot {
+          background: #f56760;
+        }
+        .blue-dot {
+          background: #347eff;
+        }
+      }
     }
   }
   .el-footer {
